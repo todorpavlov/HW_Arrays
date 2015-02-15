fa27550d61f795e5c2b505b3811987960253a96e
Todor Pavlov <todor@recitybg.com>
2015-02-15 14:44:44 +0200

    fint sum in array

---
+++ 09.FrequentNumber/FrequentNumber.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+    class FrequentNumber
+    {
+        static void Main()
+        {
+            Console.WriteLine("Frequent Number");
+            Console.WriteLine();
+            Console.Write("Enter the lenght of the array: ");
+            int n = int.Parse(Console.ReadLine());
+            
+            int m = 0;
+            int currentCount = 1;
+            int newCount = 1;
+          
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter element {0}: ", i);
+                arr[i] = int.Parse(Console.ReadLine());
+
+           
+            }
+            Array.Sort(arr);
+            for (int i = 0; i < n - 1; i++)
+            {
+             
+                    if (arr[i] == arr[i + 1])
+                    {
+                        currentCount++;
+                    }
+                    else
+                    {
+                        currentCount = 1;
+                    }
+                    if (currentCount > newCount)
+                    {
+                        newCount = currentCount;
+                        m = arr[i];
+                    }
+            }
+            if (newCount > 1)
+            {
+                Console.WriteLine("The most frequent number is {0} ({1} times).", m, newCount);
+            }
+            else
+            {
+                Console.WriteLine("There aren't any equal elements.");
+            }
+        }
+    }
+
